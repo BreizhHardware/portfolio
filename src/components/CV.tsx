4789bc2aed8cf171aea7fbd8b8ae09818cf2ca80
Félix MARQUET <fpmovie4@gmail.com>
2024-06-20 11:25:31 +0200

    Add CV

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -5,7 +5,7 @@ function CV(){
     return (
         <div className="max-w-4xl mx-auto mt-16 flex justify-center items-center flex-col gap-1">
             <p className="text-2xl md:text-4xl font-bold text-center text-gray-800 dark:text-gray-200">Mon CV</p>
-            <iframe src="/CV%20Félix%20MARQUET.pdf" width="70%" height="940px"></iframe>
+            <iframe src="/CV%20Félix%20MARQUET.pdf" className="w-full md:w-3/4 h-64 md:h-96 lg:h-screen"></iframe>
         </div>
     );
 }
