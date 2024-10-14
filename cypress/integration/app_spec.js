45af4203c4ec7821ecef8a759b06a03cc89e0d60
BreizhHardware <fpmovie4@gmail.com>
2024-10-14 16:49:02 +0200

    Expand unit test and fix margin issue

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -6,7 +6,7 @@ describe('App Component', () => {
 
     it('renders the app and checks if CSS is loaded', () => {
         cy.get('[data-testid="root"]', { timeout: 10000 }).should('exist');
-        cy.get('[data-testid="root"]').should('have.class', 'min-h-screen py-10 px-3 sm:px-5 bg-gray-100 dark:bg-gray-900');
+        cy.get('[data-testid="root"]').should('have.class', 'min-h-screen py-10 bg-gray-100 dark:bg-gray-900 m-0');
     });
 
     it('should match the previous screenshot', () => {
