5c0b168c2e557c46fe9f5b66e7bcff1540c8ff57
Félix MARQUET <fpmovie4@gmail.com>
2024-11-29 16:48:20 +0100

    Fix display for Skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,7 +14,7 @@ interface SkillsProps {
 
 function Skills({skills}: SkillsProps) {
     return(
-        <div className="flex flex-col sm:flex-row align-center justify-center max-w-8xl mx-auto mt-8 gap-5 flex-wrap">
+        <div className="flex flex-row align-center justify-center max-w-8xl mt-8 gap-5 flex-wrap skill-margin">
             {skills.map((skill: Skill) => {
                 return (
                     <SkillCard skillName={skill.skillName} skillLevel={skill.skillLevel}/>
